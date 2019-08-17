9daad02cf1b7c2abc1566f134a02fa06742a3784
Sellieee <sellieee@hotmail.com>
2019-08-17 23:50:33 +1000

    added on click of projects to open externally and also added hover text to notify viewers that they can click to see the project

--- javascript/app.js
+++ javascript/app.js
@@ -1,3 +1,20 @@
 $(document).ready(function () {
-
+    $(".img-1").on("click", function () {
+        window.open("https://sellieee.github.io/SC_200/")
+    });
+    $(".img-2").on("click", function () {
+        window.open("https://sellieee.github.io/Train-Activity/")
+    });
+    $(".img-3").on("click", function () {
+        window.open("https://sellieee.github.io/Bootstrap-Portfolio/")
+    });
+    $(".img-4").on("click", function () {
+        window.open("https://sellieee.github.io/TriviaGame/")
+    });
+    $(".img-5").on("click", function () {
+        window.open("https://sellieee.github.io/Psychic-Game/")
+    });
+    $(".img-6").on("click", function () {
+        window.open("https://sellieee.github.io/Responsive-Portfolio/")
+    });
 })
